3793dd63eeadf34718f36928215675c3d0714954
wslmacieira <wslmacieira@outlook.com>
2019-09-19 09:56:06 -0300

    Updates

---
+++ precache-manifest.4a7f4fba00b74feadaa84cf58fc8d533.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "437f7649e36c0ab74872c8f4c97afc56",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f3efcf7584de782a8ae9",
+    "url": "/monsters-rolodex/static/css/main.3ac166c6.chunk.css"
+  },
+  {
+    "revision": "64bc1093a98f7bfeeaf3",
+    "url": "/monsters-rolodex/static/js/2.763fb522.chunk.js"
+  },
+  {
+    "revision": "f3efcf7584de782a8ae9",
+    "url": "/monsters-rolodex/static/js/main.8f142d36.chunk.js"
+  },
+  {
+    "revision": "cf60efee4cc4f36414c2",
+    "url": "/monsters-rolodex/static/js/runtime~main.8759d1dd.js"
+  }
+]);
