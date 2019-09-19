bd5639bae42cf5055b7c5aa6d1c057564edfe0d2
wslmacieira <wslmacieira@outlook.com>
2019-09-19 08:53:30 -0300

    Search Box Component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import React, { Component } from 'react';
 import { CardList } from './components/CardList';
 
 import './App.css';
+import { SearchBox } from './components/SearchBox';
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
 
@@ -20,9 +22,18 @@ class App extends Component {
   }
 
   render() {
+    const { monsters,  searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+    monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <SearchBox 
+        type='search'
+        placeholder='search monsters'
+        handleChange={e => this.setState({searchField: e.target.value})}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
