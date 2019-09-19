18cc5aef24171c6df0142f97d6113960cd8b7b30
wslmacieira <wslmacieira@outlook.com>
2019-09-18 21:40:17 -0300

    Fetching Content

--- src/App.js
+++ src/App.js
@@ -6,28 +6,22 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Frankstein',
-          id: 1
-        },
-        {
-          name: 'Dracula',
-          id: 2
-        },
-        {
-          name: 'Zombie',
-          id: 3
-        },
-      ]
+      monsters: []
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users}))
   }
 
   render() {
     return (
       <div className="App">
         {
-          this.state.monsters.map((monster) => <h1 key={monster.id}>{monster.name}</h1>)
+          this.state.monsters.map((monster) => 
+          <h1 key={monster.id}>{monster.name}</h1>)
         }
       </div>
     );
